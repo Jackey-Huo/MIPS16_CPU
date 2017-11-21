201ca9a90e77465c63cf150fa7f1f2cc359b74ca
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2017-11-21 12:33:57 +0800

    flash test opened, but failed with multiple drivers error

--- pa.fromNetlist.tcl
+++ pa.fromNetlist.tcl
@@ -1,10 +1,11 @@
 
-# PlanAhead Launch Script for Post-Synthesis floorplanning, created by Project Navigator
+# PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name cpu -dir "D:/CPU/cpu/planAhead_run_1" -part xc3s1200efg320-4
+create_project -name cpu -dir "D:/CPU/cpu/planAhead_run_2" -part xc3s1200efg320-4
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "D:/CPU/cpu/cpu.ngc" [ get_property srcset [ current_run ] ]
+set_property edif_top_file "D:/CPU/cpu/test_flash.ngc" [ get_property srcset [ current_run ] ]
 add_files -norecurse { {D:/CPU/cpu} }
-set_property target_constrs_file "cpu.ucf" [current_fileset -constrset]
-add_files [list {cpu.ucf}] -fileset [get_property constrset [current_run]]
+set_param project.pinAheadLayout  yes
+set_property target_constrs_file "test_flash.ucf" [current_fileset -constrset]
+add_files [list {test_flash.ucf}] -fileset [get_property constrset [current_run]]
 link_design
--- test_code/test_flash.vhd
+++ test_code/test_flash.vhd
@@ -19,6 +19,10 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library basic;
+use basic.helper.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,35 +34,87 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity test_flash is
+	port(
+			clk : in std_logic;
+			rst : in std_logic;
+
+			flash_addr : out std_logic_vector (22 downto 1);
+			flash_data : out std_logic_vector (15 downto 0);
+			flash_byte : out std_logic;--BYTE#
+			flash_vpen : out std_logic;
+			flash_ce : out std_logic;
+			flash_oe : out std_logic;
+			flash_we : out std_logic;
+			flash_rp : out std_logic;
+			
+			led : out std_logic_vector(15 downto 0)
+		);
+		
 end test_flash;
 
 architecture Behavioral of test_flash is
 
 component flash_io is
-    Port ( addr : in  STD_LOGIC_VECTOR (22 downto 1);
-           data_in : in  STD_LOGIC_VECTOR (15 downto 0);
-           data_out : out  STD_LOGIC_VECTOR (15 downto 0);
-			  clk : in std_logic;--Ëæ±ãÊ²Ã´Ê±ÖÓ
-			  reset : in std_logic;
-			  
-			  flash_byte : out std_logic;--BYTE#
-			  flash_vpen : out std_logic;
-			  flash_ce : out std_logic;
-			  flash_oe : out std_logic;
-			  flash_we : out std_logic;
-			  flash_rp : out std_logic;
-			  --flash_sts : in std_logic;
-			  flash_addr : out std_logic_vector(22 downto 1);
-			  flash_data : inout std_logic_vector(15 downto 0);
-			  
-           ctl_read : in  STD_LOGIC;
-           ctl_write : in  STD_LOGIC;
-			  ctl_erase : in STD_LOGIC
+    Port (
+			addr : in  STD_LOGIC_VECTOR (22 downto 1);
+			data_in : in  STD_LOGIC_VECTOR (15 downto 0);
+			data_out : out  STD_LOGIC_VECTOR (15 downto 0);
+			clk : in std_logic;
+			reset : in std_logic;
+
+			flash_byte : out std_logic;--BYTE#
+			flash_vpen : out std_logic;
+			flash_ce : out std_logic;
+			flash_oe : out std_logic;
+			flash_we : out std_logic;
+			flash_rp : out std_logic;
+			--flash_sts : in std_logic;
+			flash_addr : out std_logic_vector(22 downto 1);
+			flash_data : out std_logic_vector(15 downto 0);
+
+			ctl_read : in  std_logic;
+			ctl_write : in  std_logic;
+			ctl_erase : in std_logic
 	);
 end component;
 
+	signal bl_flash_addr	: std_logic_vector (22 downto 1) := "0000000000000000000000";
+	signal bl_flash_datain	: std_logic_vector (15 downto 0) := zero16;
+	signal bl_flash_dataout	: std_logic_vector (15 downto 0) := zero16;
+	signal temp	: std_logic_vector (15 downto 0) := zero16;
+	signal clk_flash 		: std_logic						 := '0';
 begin
+	read_flash : flash_io port map (
+		addr => bl_flash_addr,
+		data_in => bl_flash_datain,
+		data_out => bl_flash_dataout,
+		clk => clk,
+		reset => rst,
+		flash_byte => flash_byte,
+		flash_vpen => flash_vpen,
+		flash_ce => flash_ce,
+		flash_oe => flash_oe,
+		flash_we => flash_we,
+		flash_rp => flash_rp,
+		--flash_sts => flash_sts,
+		flash_addr => flash_addr,
+		flash_data => flash_data,
+		ctl_read => '0',
+		ctl_write => '1',
+		ctl_erase => '1'
+	);
+	process(clk, rst)
+	begin
+		if (rst = '0') then
+            bl_flash_addr <= "0000000000000000000000";
+			bl_flash_datain <= zero16;
+			bl_flash_dataout <= zero16;
+        elsif ( clk'event and clk='1' ) then
+            bl_flash_addr <= bl_flash_addr + 1;
+			--led <= bl_flash_dataout;
+        end if;
+	end process;
 
-
+	
 end Behavioral;
 
