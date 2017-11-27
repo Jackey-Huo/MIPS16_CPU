22100f27252e3376b5f8b44d6065bba149729041
Jackey-Huo <767327611@qq.com>
2017-11-27 10:00:53 +0800

    adjust testbench file from tb branch

--- testbench/basic7tb.vhd
+++ testbench/basic7tb.vhd
@@ -76,11 +76,11 @@ ARCHITECTURE behavior OF basic7tb IS
    signal seri_tsre : std_logic := '0';
    signal instruct : std_logic_vector(15 downto 0) := (others => '0');
 
-	--BiDirs
+    --BiDirs
    signal data_ram1 : std_logic_vector(15 downto 0);
    signal data_ram2 : std_logic_vector(15 downto 0);
 
- 	--Outputs
+    --Outputs
    signal addr_ram1 : std_logic_vector(17 downto 0);
    signal OE_ram1 : std_logic;
    signal WE_ram1 : std_logic;
@@ -95,7 +95,6 @@ ARCHITECTURE behavior OF basic7tb IS
    signal dyp1 : std_logic_vector(6 downto 0);
    signal led : std_logic_vector(15 downto 0);
 
-	signal r0, r1, r2, r3, r4, r5, r6, r7 : std_logic_vector(15 downto 0) := x"0000";
   signal pc_real : std_logic_vector(15 downto 0) := x"0000";
    -- Clock period definitions
    constant clk_period : time := 10 ns;
@@ -103,7 +102,7 @@ ARCHITECTURE behavior OF basic7tb IS
   signal temp : std_logic_vector(15 downto 0) := x"0000";
 BEGIN
  
-	-- Instantiate the Unit Under Test (UUT)
+    -- Instantiate the Unit Under Test (UUT)
    uut: cpu PORT MAP (
           clk => clk,
           rst => rst,
@@ -131,83 +130,66 @@ BEGIN
    -- Clock process definitions
    clk_process :process
    begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
    end process;
  
 
    -- Stimulus process
    stim_proc: process
-	begin
+    begin
       -- hold reset state for 100 ns.
-      wait for 70 ns;	
+      wait for 70 ns;   
 
       wait for clk_period*10;
 
-		rst <= '0';
-		rst <= '0';
-		wait for clk_period;
-		rst <= '1';
-		wait for clk_period;
+        rst <= '0';
+        rst <= '0';
+        wait for clk_period;
+        rst <= '1';
+        wait for clk_period;
 
-		instruct <= "0110100000001000"; -- [0000] LI R0 0x08
-		wait for clk_period; --IF
-		instruct <= "0110100100001100"; -- [0001] LI R1 0x0C
-		wait for clk_period; --ID
-		instruct <= "0110101100000100"; -- [0002] LI R3 0x04
-		wait for clk_period; --EX
-		instruct <= "1110000000111001"; -- [0003] ADDU R0 R1 R6       R0=0x08  R1=0x0C
-		wait for clk_period; --MEM
-		instruct <= "1110011000111101"; -- [0004] ADDU R6 R1 R7       R1=0x0C  R6=0x14
-		wait for clk_period; --WB
-    assert r0 = x"0008" report "[0000] Failed" severity error;
-		instruct <= "0100100101010000"; -- [0005] ADDIU R1 0x50
-		wait for clk_period;
-    assert r1 = x"000c" report "[0001] Failed" severity error;
-		instruct <= "0100111101010000"; -- [0006] ADDIU R7 0x50     before R7=0x20; after R7=0x70
-		wait for clk_period;
-    assert r3 = x"0004" report "[0002] Failed" severity error;
-    instruct <= "1110000001101011"; -- [0007] SUBU R0 R3 R2
-    wait for clk_period;
-    assert r6 = x"0014" report "[0003] Failed" severity error;
-		instruct <= "0011001001010000"; -- [0008] SLL R2 R2 4
-		wait for clk_period;
-    assert r7 = x"0020" report "[0004] Failed" severity error;
-		instruct <= "1001100000100001"; -- [0009] LW R0 R1 0x01
-		wait for clk_period;
-    assert r1 = x"005c" report "[0005] Failed" severity error;
-		instruct <= "0100100101010000"; -- [000A] ADDIU R1 0x50
-		wait for clk_period;
-    assert r7 = x"0070" report "[0006] Failed" severity error;
-		instruct <= "1101100000100001"; -- [000B] SW R0 R1 0x01
-		wait for clk_period;
-    assert r2 = x"0004" report "[0007] Failed" severity error;
-		instruct <= "0010100000001000"; -- [000C] BNEZ R0 0x80
-    temp <= pc_real + x"0080";
-		wait for clk_period;
-    assert r2 = x"0040" report "[0008] Failed" severity error;
-		instruct <= "0000100000000000"; -- NOP
-		wait for clk_period;
+        instruct <= "0110100000001000"; -- [0000] LI R0 0x08
+        wait for clk_period; --IF
+        instruct <= "0110100100001100"; -- [0001] LI R1 0x0C
+        wait for clk_period; --ID
+        instruct <= "0110101100000100"; -- [0002] LI R3 0x04
+        wait for clk_period; --EX
+        instruct <= "1110000000111001"; -- [0003] ADDU R0 R1 R6       R0=0x08  R1=0x0C
+        wait for clk_period; --MEM
+        instruct <= "1110011000111101"; -- [0004] ADDU R6 R1 R7       R1=0x0C  R6=0x14
+        wait for clk_period; --WB
+        instruct <= "0100100101010000"; -- [0005] ADDIU R1 0x50
+        wait for clk_period;
+        instruct <= "0100111101010000"; -- [0006] ADDIU R7 0x50     before R7=0x20; after R7=0x70
+        wait for clk_period;
+        instruct <= "1110000001101011"; -- [0007] SUBU R0 R3 R2
+        wait for clk_period;
+        instruct <= "0011001001010000"; -- [0008] SLL R2 R2 4
+        wait for clk_period;
+        instruct <= "1001100000100001"; -- [0009] LW R0 R1 0x01
+        wait for clk_period;
+        instruct <= "0100100101010000"; -- [000A] ADDIU R1 0x50
+        wait for clk_period;
+        instruct <= "1101100000100001"; -- [000B] SW R0 R1 0x01
+        wait for clk_period;
+        instruct <= "0010100000001000"; -- [000C] BNEZ R0 0x80
+        wait for clk_period;
+        instruct <= "0000100000000000"; -- NOP
+        wait for clk_period;
     -- [0009]
-		instruct <= "0110100010111111"; -- LI R0 0xBF
-		wait for clk_period;
+        instruct <= "0110100010111111"; -- LI R0 0xBF
+        wait for clk_period;
     -- [000A]
-		instruct <= "0011000000010000"; -- SLL R0 R0 0x00
-		wait for clk_period;
+        instruct <= "0011000000010000"; -- SLL R0 R0 0x00
+        wait for clk_period;
     -- [000B]
-		instruct <= "1101100000100001"; -- SW R0 R1 0x01
-		wait for clk_period;
-    assert pc_real = temp report "[000C] Failed" severity error;
-    wait for clk_period;
-    wait for clk_period;
-    wait for clk_period;
-    wait for clk_period;
-    wait for clk_period;
+        instruct <= "1101100000100001"; -- SW R0 R1 0x01
+        wait for clk_period;
 
-
-		wait;
+        wait;
    end process;
 
 END;
